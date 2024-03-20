d2bd4933971ef3c575fb81e00788e5a49404b990
online lab <online lab@DESKTOP-CQI1826>
2024-03-20 12:01:06 +0530

    push-1

---
+++ employee/src/employee/loginpage.java
@@ -0,0 +1,125 @@
+package employee;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+
+public class loginpage {
+
+	private JFrame frame;
+	private JTextField tb1;
+	private JPasswordField p1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginpage window = new loginpage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public loginpage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("LOGIN PAGE");
+		lblNewLabel.setForeground(new Color(255, 0, 0));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(183, 22, 114, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("NAME");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(78, 65, 46, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("PASSWORD");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_2.setBounds(78, 108, 80, 14);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		tb1 = new JTextField();
+		tb1.setBounds(168, 62, 86, 20);
+		frame.getContentPane().add(tb1);
+		tb1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("SUBMIT");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name=tb1.getText();
+				String password=p1.getText();
+				try
+				{
+					Connection con=DriverManager.getConnection
+							("jdbc:mysql://localhost:3306/mydb","root","mrec");
+					PreparedStatement stn=con.prepareStatement
+							("select name,password from users where name=? and password=?");
+					stn.setString(1,name);
+					stn.setString(2,password);
+					ResultSet rs=stn.executeQuery();
+					if(rs.next())
+					{
+						JOptionPane.showMessageDialog(btnNewButton,"valid user");
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(btnNewButton,"invalid user");
+					}
+				}
+				catch(SQLException el)
+				{
+					el.printStackTrace();
+				}
+			}			
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton.setBounds(140, 163, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		p1 = new JPasswordField();
+		p1.setBounds(168, 105, 86, 20);
+		frame.getContentPane().add(p1);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\online lab\\Downloads\\43db4682a57bbddf864719d9403919eb.jpg"));
+		lblNewLabel_3.setBounds(0, 0, 434, 261);
+		frame.getContentPane().add(lblNewLabel_3);
+	}
+}
